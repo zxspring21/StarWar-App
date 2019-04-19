1f1a18aeaf08cf0b20927ee609c3098d60a2b283
zxspring21 <zxgpkm@gmail.com>
2019-04-19 10:37:42 +0800

    updating current progress

--- Game/ViewController.swift
+++ Game/ViewController.swift
@@ -8,12 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    //MARK: Properties
+    @IBOutlet weak var missle1ImageView: UIImageView!
+    @IBOutlet weak var missle2ImageView: UIImageView!
+    @IBOutlet weak var missle3ImageView: UIImageView!
+    @IBOutlet weak var fireImageView: UIImageView!
+    @IBOutlet weak var planeImageView: UIImageView!
+    @IBOutlet weak var scoretextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        scoretextField.delegate = self
     }
+    
+    //Mark: Actions
+    @IBAction func selectmissleItemLibrary(_ sender: UITapGestureRecognizer) {
+    }
+    
 
 
 }
---
+++ Game/missleController.swift
@@ -0,0 +1,42 @@
+//
+//  missleController.swift
+//  Game
+//
+//  Created by Portran Lu on 2019/4/9.
+//  Copyright © 2019 Velodash. All rights reserved.
+//
+
+import Foundation
+import os.log
+class missleController: NSObject {
+    
+    /* NSCoding
+    func encode(with aCoder: NSCoder) {
+        <#code#>
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        <#code#>
+    }
+    */
+     //MARK: Properties
+    struct PropertyKey {
+        static let missle1 = "missle1"
+        static let missle2 = "missle2"
+        static let missle3 = "missle3"
+    }
+    /*
+    var missle1: UIImage?
+    var missle2: UIImage?
+    var missle3: UIImage?
+    */
+    //MARK: Initialization
+    /*
+    init?(missle1: UIImage?, missle2: UIImage?, missle3: UIImage?) {
+        self.missle1 = missle1
+        self.missle2 = missle2
+        self.missle3 = missle3
+    }
+    */
+
+}
